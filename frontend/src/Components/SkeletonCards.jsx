054bd77fd150c8ed80b818ccaf42a21d81b761ba
Vikram <saivikram@gmail.com>
2023-12-08 19:11:53 +0530

    final

--- frontend/src/Components/SkeletonCards.jsx
+++ frontend/src/Components/SkeletonCards.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
-import Skeleton from 'react-loading-skeleton'
-import 'react-loading-skeleton/dist/skeleton.css'
+import React from 'react';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
 
+const SkeletonCards = ({ cards }) => {
+  const allCards = Array(cards).fill(0);
 
-const SkeletonCards = ({cards}) => {
-  const allCards = Array(cards).fill(0)
   return (
     <div className='w-full p-1 md:p-2 lg:p-1  md:[100%] lg:w-[80%] mt-20  overflow-hidden mb-15'>
-      {
-        allCards.map((idx)=>{
-          return(
-            <div key={idx} className='bg-white m-1 flex flex-col'>
+      {allCards.map((_, index) => {
+
+        return (
+          <div key={index} className='bg-white m-1 flex flex-col'>
             <div className='p-4 flex justify-start items-center border-b'>
-                <Skeleton circle width={50} height={50} />
-                <div className='ml-4 w-[20%]'>
-                  <Skeleton  />
-                </div>
+              <Skeleton circle width={50} height={50} />
+              <div className='ml-4 w-[20%]'>
+                <Skeleton />
+              </div>
             </div>
             <div className='p-4 h-[350px]'>
               <Skeleton height={300} />
             </div>
           </div>
-          )
-        })
-      }
-  
+        );
+      })}
     </div>
-    
-
-  )
-}
+  );
+};
 
 export default SkeletonCards;
